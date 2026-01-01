dd01f7c1c2faf0c39af8a51693848b3a884daf0a
Ardinnn <ardinsholihin33@gmail.com>
2026-01-02 01:48:47 +0700

    fix tabel bisa digeser di hp

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12 pb-32">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white overflow-x-auto shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
                 <div class="mb-6 flex justify-between items-center">
@@ -106,11 +106,9 @@
         </div>
     </div>
 
-    <div style="margin-top: 50px; text-align: center; color: red;">
-            Halaman Update V3
-        </div>
-        <br><br><br><br><br><br><br><br><br><br>
+    <div style="height: 150px;"></div>
         
-    </div> </div>
+    </div>
+</div>
 
 </x-app-layout>
